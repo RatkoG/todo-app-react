9cc9b237c204d79ae74710b59a5d39b0ba21da85
Ratko Gjurichanin <ratko.gjurichanin@gmail.com>
2020-04-10 09:04:25 -0600

    Initial Setup

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+const StyledText = styled.div`
+  color: red;
+`;
+
+const App = () => {
+  return <StyledText>TODO APP</StyledText>;
+};
+
+export default App;
